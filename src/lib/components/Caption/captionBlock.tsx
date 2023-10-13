334e82157a986f44c32a0628030ebf7553a2f434
chanhee kim <hbnation.it@gmail.com>
2023-10-13 21:53:26 +0900

    ENH: caption sass to styled component

--- src/lib/components/Caption/captionBlock.tsx
+++ src/lib/components/Caption/captionBlock.tsx
@@ -1,11 +1,11 @@
 import RichTexts from '../RichText/richTexts'
 import { RichText } from '../../types/blockType'
-import styles from './captionBlock.module.scss'
+import { Container } from './styled'
 
 export default function CaptionBlock({ caption }: { caption: RichText[] }) {
   return (
-    <div className={styles.caption}>
+    <Container>
       <RichTexts richTexts={caption} />
-    </div>
+    </Container>
   )
 }
