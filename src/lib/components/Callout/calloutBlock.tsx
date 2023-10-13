9f414184a885f5ab0f128b273b3ad38c85f31242
chanhee kim <hbnation.it@gmail.com>
2023-10-13 21:52:18 +0900

    ENH: callout sass to styled component

--- src/lib/components/Callout/calloutBlock.tsx
+++ src/lib/components/Callout/calloutBlock.tsx
@@ -1,16 +1,15 @@
 import { Callout } from '../../types/blockType'
 import IconBlock from '../Icon/iconBlock'
 import RichTexts from '../RichText/richTexts'
-import CalloutColor from './calloutColor'
-import styles from './calloutBlock.module.scss'
+import { Container } from './styled'
 
 export default function CalloutBlock({ block }: { block: Callout }) {
   return (
-    <div className={`${CalloutColor(block.callout.color)} ${styles.callout}`}>
+    <Container className={block.callout.color}>
       <IconBlock icon={block.callout.icon} />
-      <div className={styles.content}>
+      <div className="content">
         <RichTexts richTexts={block.callout.rich_text} />
       </div>
-    </div>
+    </Container>
   )
 }
